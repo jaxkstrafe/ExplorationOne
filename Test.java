072f1b440521c26dd854e6a2818f2b5b30fa9afc
benstewart1212 <109236686+benstewart1212@users.noreply.github.com>
2024-02-08 10:17:21 -0600

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,33 @@
+/*
+ * 
+ * Group Members: Jack Dalton, Juan Robledo, Ben Stewart
+ * Assignment: Exploration One
+ * Instructor: Becnel
+ * Course: 3342 - 001
+ * Date: 2/8/2024
+ * 
+ * Objective: Create two algorithms using the recursive 
+ * and iterative methods to be compared using the 
+ * file Test.java
+ * 
+ * Equation to compare:
+ * f(0) = 0
+ * f(1) = 2
+ * f(2) = 4
+ * F(n) = F(n - 1) + nF(n - 2) - 2F(n - 3) + 5
+ */
+
+public class Compare {
+    public static void main(String[] arg){
+        //code here
+    }// end main
+
+
+    public static int recursiveF(){
+        // code here
+    }// end recF
+
+    public static int iterativeF(){
+        // code here
+    }//end iterF
+}
