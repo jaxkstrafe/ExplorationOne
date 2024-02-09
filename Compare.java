2bbd75a3072c0ceb97822594a8d4a02647cb601d
benstewart1212 <109236686+benstewart1212@users.noreply.github.com>
2024-02-09 11:55:57 -0600

    Update Compare.java
    All code added for algorithms

--- Compare.java
+++ Compare.java
@@ -18,16 +18,51 @@
  */
 
 public class Compare {
-    public static void main(String[] arg){
-        //code here
-    }// end main
 
-
-    public static int recursiveF(){
-        // code here
+    public static int recursiveF(int n) {
+        if (n == 0) {
+            return 0; // base case a
+        } else if (n == 1) {
+            return 2; // base case b
+        } else if (n == 2) {
+            return 4; // base case c
+        } else {
+            // for all values n > 3 uses references to previous values
+            return recursiveF(n - 1) + (n * recursiveF(n - 2)) - (2 * recursiveF(n - 3)) + 5;
+        }
     }// end recF
 
-    public static int iterativeF(){
-        // code here
-    }//end iterF
+    public static int iterativeF(int n) {
+       // create variables to hold base cases
+       int fZero = 0;
+       int fOne = 2;
+       int fTwo = 4;
+
+       //set values for if base case is passed in 
+       // copied from recursiveF
+       if (n == 0) {
+        return 0; // base case a
+    } else if (n == 1) {
+        return 2; // base case b
+    } else if (n == 2) {
+        return 4; // base case c
+    }
+
+    //function > 2 so we can start w
+    // the values at 3 and build up to 
+    // the answer. 
+    // This is a note bc I solved w an array
+    // but rewrote it without. 
+    int value = 0;
+
+    for(int i = 3; i <= n; i++){
+        value = fTwo + (i * fOne) - (2 * fZero) + 5;
+        fZero = fOne;
+        fOne = fTwo;
+        fTwo = value;
+        
+    }// end for loop
+
+    return value;
+    }// end iterF
 }
